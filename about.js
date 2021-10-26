8c2d886b0158d9d9877796bdc3f2e69f0919f835
Aryan <89679204+Roy-com@users.noreply.github.com>
2021-10-26 13:21:05 +0530

    Update about.js

--- about.js
+++ about.js
@@ -36,9 +36,9 @@ document.querySelector(".click_twit").addEventListener("click", (e) => {
   e.preventDefault();
   window.open("https://twitter.com/aryan3865?s=08");
 });
-document.querySelector(".click_insta").addEventListener("click", (e) => {
+document.querySelector(".click_link").addEventListener("click", (e) => {
   e.preventDefault();
-  window.open("https://www.instagram.com/aryanroy.tony?r=nametag");
+  window.open("https://www.linkedin.com/in/aryan-kumar-88515121a");
 });
 document.querySelector(".click_insta").addEventListener("click", (e) => {
   e.preventDefault();
